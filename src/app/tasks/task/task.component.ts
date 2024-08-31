82f99e3abc4a67c3040b276264ff7b6b3d24ea3e
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-31 09:33:54 +0200

    feat: pass dummy tasks to task component

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DUMMY_TASKS } from '../../dummy-tasks';
+
+type Task = (typeof DUMMY_TASKS)[number];
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-
+  @Input({ required: true }) task!: Task;
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { DUMMY_TASKS } from '../dummy-tasks';
+
+type Tasks = {
+  userId: string;
+  name: string;
+};
 
 @Component({
   selector: 'app-tasks',
@@ -9,5 +15,8 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) tasks!: Tasks;
+  get selectedUserTasks() {
+    return DUMMY_TASKS.filter((task) => task.userId === this.tasks.userId);
+  }
 }
