e2b846d2c31348b3f0ef83e8a230b0df4609de81
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-31 10:19:46 +0200

    feat: add new task to tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,22 +2,29 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from '../dummy-tasks';
 import { Tasks } from './taks.model';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   dummyTasks = DUMMY_TASKS;
   @Input({ required: true }) tasks!: Tasks;
+  isAddingTask = false;
+
   get selectedUserTasks() {
     return this.dummyTasks.filter((task) => task.userId === this.tasks.userId);
   }
 
   onCompleteTask(id: string) {
     this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
