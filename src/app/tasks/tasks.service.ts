cc402cb05ec9ea434127facca39e7c3c47fc0dda
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-31 15:37:26 +0200

    feat: save tasks on localStorage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -7,6 +7,12 @@ import { NewTaskData } from './tasks.model';
 })
 export class TasksService {
   private tasks = DUMMY_TASKS;
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
 
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
@@ -18,9 +24,15 @@ export class TasksService {
       userId: userId,
       ...taskData,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
